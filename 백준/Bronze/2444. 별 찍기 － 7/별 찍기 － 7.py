5b3aac72637614338e37bff4db15d47f85c7ad47
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2024-10-16 11:31:15 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,20 @@
+def print_pyramid(n: int):
+    # TODO: implement this function
+    for i in range(1, n+1):
+        for j in range(n-i):
+            print(' ', end='')
+        for k in range(1, 2*i):
+            print('*', end='')
+        print()
+
+
+def print_inverted_pyramid(n: int):
+    # TODO: implement this function
+    for i in range(1,n):
+        print(" "*(i) + "*"*(2*(n-i)-1))
+
+
+if __name__ == "__main__":
+    N = int(input())
+    print_pyramid(N)
+    print_inverted_pyramid(N)

f39784baa6fd8a7b5b3798c54f7df3eee58121c4
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-20 09:25:50 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,8 +1,9 @@
 function solution(a, b) {
     var answer = 0;
-    let sol1 = Number(String(a)+String(b));
-    let sol2 = Number(String(b)+String(a));
+//     let sol1 = Number(String(a)+String(b));
+//     let sol2 = Number(String(b)+String(a));
     
-    answer = sol1>=sol2?sol1:sol2;
+//     answer = sol1>=sol2?sol1:sol2;
+    answer = Math.max(Number(`${a}${b}`), Number(`${b}${a}`));
     return answer;
 }

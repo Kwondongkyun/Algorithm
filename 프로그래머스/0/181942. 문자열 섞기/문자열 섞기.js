63a86606c8e1133f9d1099716c1f224df970f020
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-20 09:04:22 +0900

    [level 0] Title: 문자열 섞기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,7 +1,8 @@
 function solution(str1, str2) {
     var answer = '';
-    for(let i=0;i<str1.length;i++){
-        answer+=str1[i]+str2[i];
-    }
+    // for(let i=0;i<str1.length;i++){
+    //     answer+=str1[i]+str2[i];
+    // }
+    return [...str1].map((x, idx)=>x+str2[idx]).join("");
     return answer;
 }

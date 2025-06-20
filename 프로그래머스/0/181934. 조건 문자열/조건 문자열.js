de0d0e1b41bbf5ac085594781fb66f8f48c007c1
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-20 12:09:37 +0900

    [level 0] Title: 조건 문자열, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,14 +1,18 @@
 function solution(ineq, eq, n, m) {
-    if(ineq === "<"){
-        if(eq==="=")
-            return n <= m?1:0;
-        else if(eq==='!')
-            return n<m?1:0;
-    }
-    else if(ineq === ">"){
-        if(eq==="=")
-            return n >= m?1:0;
-        else if(eq==="!")
-            return n>m?1:0;
-    }
+    // if(ineq === "<"){
+    //     if(eq==="=")
+    //         return n <= m?1:0;
+    //     else if(eq==='!')
+    //         return n<m?1:0;
+    // }
+    // else if(ineq === ">"){
+    //     if(eq==="=")
+    //         return n >= m?1:0;
+    //     else if(eq==="!")
+    //         return n>m?1:0;
+    // }
+    if(eq!=="!")
+        return eval(`${n} ${ineq}${eq} ${m}`)?1:0;
+    else
+        return eval(`${n} ${ineq} ${m}`)?1:0;
 }

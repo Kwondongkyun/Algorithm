c67d499a198e10dc2788e33f5ed495170c7952ea
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-22 01:28:15 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.43 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,23 @@
+function solution(n, control) {
+    var answer = n;    
+    for(const ch of control){
+        switch(ch){
+            case "w":
+                answer+=1;
+                break;
+                
+            case "s":
+                answer-=1;
+                break;
+                
+            case "d":
+                answer+=10;
+                break;
+                
+            case "a":
+                answer-=10;
+                break;
+        }
+    }
+    return answer;
+}

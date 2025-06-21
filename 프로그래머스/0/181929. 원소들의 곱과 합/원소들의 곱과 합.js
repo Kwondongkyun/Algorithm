bb66f388de8cda61b78d14ec0e97924788415a21
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-21 22:43:42 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = 0;
+    let gob=1;
+    let hab=0;
+    for(num of num_list){
+        gob*=num;
+        hab+=num;
+    }
+    return gob>hab**2?0:1;
+}

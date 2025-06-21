65942b51fc0b5d561dbcac6c40d28106e7d41e08
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-22 01:00:15 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -2,10 +2,10 @@ function solution(num_list) {
     const last=num_list.at(-1);
     const secondLast=num_list.at(-2);
     
-    if(last>secondLast)
-        num_list.push(last-secondLast);
-    else
-        num_list.push(last*2);
+    // if(last>secondLast)
+    //     num_list.push(last-secondLast);
+    // else
+    //     num_list.push(last*2);
     
-    return num_list;
+    return [...num_list, last>secondLast?last-secondLast:last*2];
 }

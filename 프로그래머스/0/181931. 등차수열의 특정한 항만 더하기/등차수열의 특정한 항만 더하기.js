4b6a7270bcf3e0fc6c522760e08f2b6b77fe64e1
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-21 22:05:48 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,7 +1,7 @@
 function solution(a, d, included) {
     var answer = 0;
     for(let i=0;i<included.length;i++){
-        answer+=(a+d*i)*included[i];
+        answer+=(a+d*i)*Number(included[i]);
     }
     return answer;
 }

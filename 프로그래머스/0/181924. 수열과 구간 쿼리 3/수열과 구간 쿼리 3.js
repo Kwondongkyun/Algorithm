8096ffaf52431ed62c98df5808d383bdf5d0189b
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-06-22 17:21:37 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.68 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,13 @@
 function solution(arr, queries) {
     var answer = [];
-    let num=0;
-    for(let i=0;i<queries.length;i++){
-        num=arr[queries[i][0]];
-        arr[queries[i][0]]=arr[queries[i][1]];
-        arr[queries[i][1]]=num;
+    // let num=0;
+    // for(let i=0;i<queries.length;i++){
+    //     num=arr[queries[i][0]];
+    //     arr[queries[i][0]]=arr[queries[i][1]];
+    //     arr[queries[i][1]]=num;
+    // }
+    for([a,b] of queries){
+        [arr[a],arr[b]]=[arr[b],arr[a]];
     }
     return arr;
 }
